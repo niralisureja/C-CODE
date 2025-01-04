89e050edafac1dcc7a818b1ee40ae7073847fa97
Nirali Sureja <145830186+niralisureja@users.noreply.github.com>
2025-01-04 21:21:29 +0530

    Time: 55 ms (30.99%) | Memory: 48.8 MB (60.09%) - LeetSync

---
+++ 2732-counter/counter.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+
+function createCounter(start) {
+  let count = start;
+  return function () {
+    return count++;
+  };
+}
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
